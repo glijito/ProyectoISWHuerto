060d664c7e302c4f68b727c777765a320d6331d8
Luis gerardo <LGSanchezRodrigo@gmail.com>
2018-11-09 04:59:00 -0600

    Initial commit

---
+++ app/src/main/java/desarrollojhlibreros/com/proyectoiswhuerto/Activity_loguin.java
@@ -0,0 +1,13 @@
+package desarrollojhlibreros.com.proyectoiswhuerto;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_loguin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loguin);
+    }
+}
